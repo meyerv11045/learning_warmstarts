a844f1bdc587c2e858fd22b405eb3a3f5b7f1184
Vikram Meyer <vjmeyer20@gmail.com>
2023-02-23 16:05:44 -0600

    eval no warmstart and warmstart methods

--- scripts/eval_ipopt.py
+++ scripts/eval_ipopt.py
@@ -8,21 +8,16 @@ from learning_warmstarts.dataset.opt import setup_problem
 from learning_warmstarts import cfg, FCNet, setup_logging, write_obj
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--model_folder', required=True)
-parser.add_argument('-l', '--layers', default="9,256,512,1024,240")
+parser.add_argument('-s', '--save_folder', required=True)
 parser.add_argument('-n','--n', type=int, default=200)
 parser.add_argument('-ll', '--log_level', default='INFO', help='DEBUG, INFO, WARNING, ERROR')
 args = parser.parse_args()
-args.layers = [int(layer_size.strip()) for layer_size in args.layers.split(',')]
 
-save_folder = Path(f'results/{args.model_folder}-eval')
+save_folder = Path(f'results/{args.save_folder}')
 if not save_folder.exists():
     save_folder.mkdir()
 
 setup_logging(args.log_level, True, save_folder/"eval.log")
-
-model = FCNet(args.layers)
-model.load_state_dict(torch.load(f'results/{args.model_folder}/model.pt'))
 
 problem = setup_problem(cfg)
 
@@ -36,21 +31,19 @@ with torch.no_grad():
         obsts = generate_obstacles(cfg)
         problem.set_value(problem.p[4:4+3*cfg.n_obstacles], obsts)
 
-        output = model(torch.tensor(obsts)).numpy()
-        problem.set_initial(problem.x, output)
-
         try:
             solution = problem.solve()
             iters = solution.stats()["iter_count"]
             t_proc = solution.stats()["t_proc_total"]
             t_wall = solution.stats()["t_wall_total"]
-            logging.info('solved %f in %f s', iters, t_wall)
+            logging.info('%f iters in %f s', iters, t_wall)
             all_iters.append(iters)
             all_t_proc.append(t_proc)
             all_t_wall.append(t_wall)
 
         except RuntimeError as e:
-            logging.info('error %s', e)
+            logging.warning('error %s', e)
+            logging.warning('obstacles: %s', str(list(obsts)))
             error_during_solve += 1
 
 def analyze(lst, caption=''):
@@ -59,9 +52,10 @@ def analyze(lst, caption=''):
     logging.info('%s avg: %f std: %f', caption, avg, std)
     return avg, std
 
-logging.info('%d randomly generated problems: ', len(all_iters))
+logging.info('%d randomly generated problems', len(all_iters))
+logging.info('%d warmstarts resulted in infeasible problems', error_during_solve)
 analyze(all_iters, 'iters')
 analyze(all_t_proc, 't_proc')
 analyze(all_t_wall, 't_wall')
 
-write_obj({'iters': all_iters, 't_procs': all_t_proc, 't_walls': all_t_wall}, save_folder/"results.pkl")
+write_obj({'iters': all_iters, 't_procs': all_t_proc, 't_walls': all_t_wall, 'infeasible': error_during_solve}, save_folder/"results.pkl")
